eb8c5b77e9daf191bf44afb0cace11233614b966
Peter Brian <wahomebrian98@gmail.com>
2023-10-06 01:26:49 +0300

    Magic calculator

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-
-def magic_calculation(a, b):
-    """Match bytecode provided by ALX"""
-    from magic_calculation_102 import add, sub
-
+def magic_calculation(a, b, c):
     if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
         return (c)
-
-    else:
-        return(sub(a, b))
+    if c > b:
+        return (a + b)
+    return (a*b - c)
