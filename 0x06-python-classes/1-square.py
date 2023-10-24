4cfd1e166676c3a92634db5a1f107c604426d271
Peter Brian <wahomebrian98@gmail.com>
2023-10-25 00:34:14 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""square class with a private attribute"""
+
+
+class Square:
+    """It has a private attribute"""
+    def __init__(self, size):
+        """Hidden attribue"""
+        self.__size = size
