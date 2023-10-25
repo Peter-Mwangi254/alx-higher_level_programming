af2a4eaf3337332654f88b759733a355a83fbeed
Peter Brian <wahomebrian98@gmail.com>
2023-10-26 00:49:30 +0300

    Update on task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a square class with methods in it"""
+
+
+class Square:
+    """init function"""
+    def __init__(self, size=0):
+        """initializes a private attribute"""
+        self.__size = size
+
+    """Retrieves the value from the hidden attribute"""
+    @property
+    def size(self):
+        return self.__size
+
+    """"sets the value retrieved to the size attribute"""
+    @size.setter
+    def size(self, value):
+        """checks if data type is int else raises TypeError"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        """Raises ValueError if size is less than 0"""
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        """initializes a public attribute"""
+        self.__size = value
+    """a public  method that determines the area of a square"""
+
+    def area(self):
+        """returns the area of square"""
+        return self.__size ** 2
