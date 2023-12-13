216e47a35951410391dd399a97ade141d95f2f7d
Peter Brian <wahomebrian98@gmail.com>
2023-12-14 00:57:22 +0300

    deletes the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
--- scripts that creates a database if it doesn't exit
+-- scripts that creates a database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- script that deletes a database if exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
