8b64b144367d454d52f129dc5070b3ef4f54a985
Peter Brian <wahomebrian98@gmail.com>
2023-12-15 12:34:23 +0300

    update

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT ts.title
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON ts.id = tsg.show_id
+INNER JOIN tv_genres AS tg
+ON tg.id = tsg.genre_id
+WHERE tg.name = "Comedy"
+ORDER BY ts.title ASC;
