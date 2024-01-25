0f0a4817b686b6f2e94fa1f9f2c2ad61c2a2e757
Peter-Mwangi254 <pm6093628@gmail.com>
2024-01-25 22:39:22 +0300

    update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-url="$1"
-curl -s -X DELETE "$url"
+curl -s -X DELETE "$1"
