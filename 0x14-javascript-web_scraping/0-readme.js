0d2a756b857577f7539ee24d9015181a455d5cba
Peter-Mwangi254 <pm6093628@gmail.com>
2024-03-26 02:24:09 -0700

    script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
